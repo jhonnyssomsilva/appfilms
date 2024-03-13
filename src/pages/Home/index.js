53f82bfd021d581e150d6671c565944201e1058c
Jhonnyssom <jhonnyssomux@gmail.com>
2024-03-13 14:00:54 -0300

    consuming an API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
 function Home() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilms() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "faef24c2d618284f0095f037bf879f6c",
+          language: "pt-BR",
+          page: 1,
+        },
+      });
+      console.log(response.data.results);
+    }
+    loadFilms();
+  }, []);
+
   return (
     <div>
       <h1>Componente Home</h1>
